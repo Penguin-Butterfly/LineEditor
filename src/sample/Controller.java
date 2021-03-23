5f59e32988c5a1cd3850630027af12f486fac166
Penguin-Butterfly <MattN55940@gmail.com>
2021-03-23 19:38:32 -0400

    Added Timestamps Made Code work

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,12 +3,18 @@ package sample;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Controller {
     public TextField text1;
     public TextField text2;
     public Button saveBtn;
     //public TextArea textbox;
-    public TableView textboxes;
+    public ListView textboxes;
     public TableColumn textColumn;
     //public String allText = "";
 
@@ -16,10 +22,19 @@ public class Controller {
         String savedText = text1.getText();
         System.out.println(savedText);
 
+        LocalDateTime DateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss a");
+        String timeStamp = DateTime.format(formatter);
+
+        /* broken Tableview code
         TableColumn<textEntry, String> textColumn = new TableColumn<>("Entry");
         textColumn.setCellValueFactory(new PropertyValueFactory<>("enteredText"));
+         */
 
-        textboxes.getItems().add(new textEntry(savedText));
+        String textToAdd = timeStamp +" :   " + savedText;
+        System.out.println(textToAdd);
+
+        textboxes.getItems().add(textToAdd);
         //allText = allText + " " + savedText;
         //textbox.setText(allText);
         text1.setText("");
